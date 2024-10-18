5e2fb900f17544b011389fe110c304f21c6a8d85
rushi <rushikesh@markiversemedia.com>
2024-10-18 17:30:01 +0530

    - added progress percentage

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -7,11 +7,15 @@ interface ProgressBarProps {
 export default function ProgressBar({progress: progress}: ProgressBarProps) {
     return(
         <>
-            <div className="w-full bg-gray-200 rounded-full h-4 mt-4">
-                <div
-                    className="bg-teal-500 h-full rounded-full"
-                    style={{width: `${progress}%`}}
-                />
+            <div className="mt-4">
+                <div className="w-full bg-gray-200 rounded-full h-5">
+                    <div
+                        className="bg-teal-500 h-full rounded-full flex justify-end items-center transition-all duration-500 ease-linear"
+                        style={{width: `${progress}%`}}
+                    >
+                        {progress >= 4 && <p className="text-white text-sm pr-5">{progress}%</p>}
+                    </div>
+                </div>
             </div>
         </>
     )
